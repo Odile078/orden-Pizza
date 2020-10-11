ad9e51a68311a0ba6fc9a3ab2e25aba3aead8533
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-10-11 19:01:14 +0200

    created a function to asses the input and print the ordered information

--- JS/index.js
+++ JS/index.js
@@ -51,3 +51,52 @@ function Totalprice(pizzaprice, pizzaquantity, pizzadelivery) {
 Totalprice.prototype.finalTotalprice = function () {
     return this.pizzadelivery + this.pizzaprice * this.pizzaquantity ;
 };
+
+// creating variables to store the price of Pizza according to the size and the delivery fee.
+var sizePrice = [1500, 900, 750]
+var deliverPrices = [0, 200];
+$(document).ready(function () {
+    
+    $('form#orderform').submit(function (event) {
+        event.preventDefault();
+        // variables for picking the inputs from the form according to the ids
+        var pizzaType = $('#pizzatype').val();
+
+        var pizzaSize = parseInt($('#pizzasize').val());
+
+        var pizzaCrust = $('#pizzacrust').val();
+
+        var pizzaTop = $('#pizzatop').val();
+
+        var pizzaquantity = parseInt($('#pizzaquantity').val());
+
+        var deliveryoption = parseInt($('#deliveryoption').val());
+        var pizzaprice = sizePrice[pizzaSize - 1];
+        var pizzaDeliveryCost = deliverPrices[deliveryoption - 1];
+
+
+        //creating new function constructors
+        newSpecification = new Specification(pizzaType, pizzaSize, pizzaCrust, pizzaTop);
+        newTotalprice = new Totalprice(pizzaprice, pizzaquantity, pizzaDeliveryCost);
+
+        //condition for printing the information of the ordered Pizza
+        //condition according to the delivery option.
+
+        // If the delivery option is to come pick the Pizza 
+        if (deliveryoption===1){
+        alert("Your order is: " + newSpecification.fullSpecification() + ".Click Ok to view your full fee.");
+        alert("your bill is: " + newTotalprice.finalTotalprice() + ".");
+       
+        }else{
+            //If the delivery option is for the Pizza to be delivered to them.
+            if(deliveryoption===2){
+                var place= prompt("Enter where you want your pizza to be delivered");
+                alert("Your order has been received and it will be delivered at " + place +" . Click Ok to view your full ordered pizza.");
+                alert("Your order is: " + newSpecification.fullSpecification() + ".Click Ok to view your full fee.");
+                alert("your bill is: " + newTotalprice.finalTotalprice() + ".Your pizza will be delevered.");
+            }
+        }
+        
+
+    });
+
